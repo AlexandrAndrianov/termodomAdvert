eb761967cb0f552bd524b5cb8d076fab1cea4e1a
AlexandrAndrianov <andatrGitHub@yandex.ru>
2014-10-09 18:26:23 +0400

    Сделал параметры для категорий на странице добавлениия объявления

--- components/bitrix/news/advert/bitrix/news.detail/.default/template.php
+++ components/bitrix/news/advert/bitrix/news.detail/.default/template.php
@@ -1,5 +1,17 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
+<?/*Группы доступа*/
+$arPERM = Array( "1", "11", "12");
+?>
+
+<?/*Определяем группы пользователя*/
+	 $arGrpUsr = $USER->GetUserGroupArray();
+	 $arRez = array_intersect($arGrpUsr, $arPERM);
+	 if(empty($arGrpUsr)){
+			echo "arGrpUsr - пустой";
+		}
+?>
+
 <?
 	if(!function_exists('inclination')){
 		function inclination($time, $arr=array("отзыв","отзыва","отзывов")) {
@@ -40,98 +52,168 @@
 </div>
 
 
-<div class="container">
-	<div class="grid-8">
-		<div class="block">
-			<div class="a-inside">
-				<div class="a-image">
-					<div class="a-image-noslider raised-corners">
-						<div class="valigner">
-							<div class="valigner-row">
-								<div class="valigner-cell">
-									<a href="">
-										<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
-											<img class="detail_picture" border="0" src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>" width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>" height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>" alt="<?=$arResult["NAME"]?>"  title="<?=$arResult["NAME"]?>" />
-										<?endif?>
-									</a>
+<?if(!empty($arResult["DETAIL_PICTURE"])):?>
+	<div class="container">
+		<div class="grid-8">
+			<div class="block">
+				<div class="a-inside">
+					<div class="a-image">
+						<div class="a-image-noslider raised-corners">
+							<div class="valigner">
+								<div class="valigner-row">
+									<div class="valigner-cell">
+										<a href="javascript:void(0);">
+											<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
+												<img class="detail_picture" border="0" src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>" width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>" height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>" alt="<?=$arResult["NAME"]?>"  title="<?=$arResult["NAME"]?>" />
+											<?endif?>
+										</a>
+									</div>
 								</div>
 							</div>
 						</div>
+						<div class="clear"></div>
 					</div>
-					<div class="clear"></div>
 				</div>
 			</div>
 		</div>
-	</div>
-	
-	<div class="grid-4">
-		<div class="block">
-			<div class="a-inside">
-				<ul class="a-stats">
-					<li>
-						<i class="fa fa-clock-o"></i>
-							<?
-								$arDate = explode(' ', $arResult["ACTIVE_FROM"]);
-							?>
-							Размещено <?=$arDate["0"]?> в <?=$arDate["1"]?>
-					</li>
-					<li>
-						<i class="fa fa-eye"></i>
-							<?=$arResult["SHOW_COUNTER"]?> 
-							<?=inclination($arResult["SHOW_COUNTER"], Array("просмотр",
-																															"просмотра",
-																															"просмотров"))?>
-					</li>
-				</ul>
-				<ul class="a-options">
-					<li class="a-price">
-						<div class="names">
-							<span>Цена: </span>
-						</div>
-						<div class="contents">
-							<?
-								$price = preg_replace('/[^0-9]/i', '',
-																			$arResult["PROPERTIES"]["PROP_PRICE"]["VALUE"]);
-							?>
-							<strong><?=$price?> р.</strong>
-						</div>
-					</li>
-					<li>
-						<div class="names">
-							<span>Влaделец: </span>
-						</div>
-						<div class="contents">
-							<strong>
+		
+		<div class="grid-4">
+			<div class="block">
+				<div class="a-inside">
+					<ul class="a-stats">
+						<li>
+							<i class="fa fa-clock-o"></i>
 								<?
-									$create = preg_replace('/\(.*\)/i','', $arResult["FIELDS"]["CREATED_USER_NAME"]);
+									$arDate = explode(' ', $arResult["ACTIVE_FROM"]);
 								?>
-								<?=$create?>
-							</strong>
-							<a href="mailto:<?=$arResult["PROPERTIES"]["PROP_EMAIL"]["VALUE"]?>">
-								<i class="fa fa-envelope-o"></i>
-								Написать сообщение
-							</a>
-						</div>
-					</li>
-					<li>
-						<div class="names">
-							<span>Телефон: </span>
-						</div>
-						<div class="contents">
-							<span>
-								<?$phone = preg_replace('/[^0-9]/i', '', 
-																					$arResult["PROPERTIES"]["PROP_PHONE"]["VALUE"]);?>
-								<?=$phone?>
-							</span>
-						</div>
-					</li>
-				</ul>
+								Размещено <?=$arDate["0"]?> в <?=$arDate["1"]?>
+						</li>
+						<li>
+							<i class="fa fa-eye"></i>
+								<?=$arResult["SHOW_COUNTER"]?> 
+								<?=inclination($arResult["SHOW_COUNTER"], Array("просмотр",
+																																"просмотра",
+																																"просмотров"))?>
+						</li>
+					</ul>
+					<ul class="a-options">
+						<li class="a-price">
+							<div class="names">
+								<span>Цена: </span>
+							</div>
+							<div class="contents">
+								<?
+									$price = preg_replace('/[^0-9]/i', '',
+																				$arResult["PROPERTIES"]["PROP_PRICE"]["VALUE"]);
+								?>
+								<strong><?=$price?> р.</strong>
+							</div>
+						</li>
+						<li>
+							<div class="names">
+								<span>Влaделец: </span>
+							</div>
+							<div class="contents">
+								<strong>
+									<?
+										$create = preg_replace('/\(.*\)/i','', $arResult["FIELDS"]["CREATED_USER_NAME"]);
+									?>
+									<?=$create?>
+								</strong>
+								<a href="mailto:<?=$arResult["PROPERTIES"]["PROP_EMAIL"]["VALUE"]?>">
+									<i class="fa fa-envelope-o"></i>
+									Написать сообщение
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="names">
+								<span>Телефон: </span>
+							</div>
+							<div class="contents">
+								<span>
+									<?$phone = preg_replace('/[^0-9]/i', '', 
+																						$arResult["PROPERTIES"]["PROP_PHONE"]["VALUE"]);?>
+									<?=$phone?>
+								</span>
+							</div>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="clear"></div>
-</div>
-					
+		<div class="clear"></div>
+	</div><!--container-->
+<?else:?>
+	<div class="container">		
+		<div class="grid-12">
+			<div class="block">
+				<div class="a-inside">
+					<ul class="a-stats">
+						<li>
+							<i class="fa fa-clock-o"></i>
+								<?
+									$arDate = explode(' ', $arResult["ACTIVE_FROM"]);
+								?>
+								Размещено <?=$arDate["0"]?> в <?=$arDate["1"]?>
+						</li>
+						<li>
+							<i class="fa fa-eye"></i>
+								<?=$arResult["SHOW_COUNTER"]?> 
+								<?=inclination($arResult["SHOW_COUNTER"], Array("просмотр",
+																																"просмотра",
+																																"просмотров"))?>
+						</li>
+					</ul>
+					<ul class="a-options">
+						<li class="a-price">
+							<div class="names">
+								<span>Цена: </span>
+							</div>
+							<div class="contents">
+								<?
+									$price = preg_replace('/[^0-9]/i', '',
+																				$arResult["PROPERTIES"]["PROP_PRICE"]["VALUE"]);
+								?>
+								<strong><?=$price?> р.</strong>
+							</div>
+						</li>
+						<li>
+							<div class="names">
+								<span>Влaделец: </span>
+							</div>
+							<div class="contents">
+								<strong>
+									<?
+										$create = preg_replace('/\(.*\)/i','', $arResult["FIELDS"]["CREATED_USER_NAME"]);
+									?>
+									<?=$create?>
+								</strong>
+								<a href="mailto:<?=$arResult["PROPERTIES"]["PROP_EMAIL"]["VALUE"]?>">
+									<i class="fa fa-envelope-o"></i>
+									Написать сообщение
+								</a>
+							</div>
+						</li>
+						<li>
+							<div class="names">
+								<span>Телефон: </span>
+							</div>
+							<div class="contents">
+								<span>
+									<?$phone = preg_replace('/[^0-9]/i', '', 
+																						$arResult["PROPERTIES"]["PROP_PHONE"]["VALUE"]);?>
+									<?=$phone?>
+								</span>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div><!--container-->
+<?endif?>				
 
 <div class="container">		
 	<div class="grid-12">
@@ -144,6 +226,22 @@
 					<ul class="a-stats">
 						<li>Номер объявления <?=$arResult["ID"]?></li>
 					</ul>
+					<?/*Делаем проверку на принадлежность группам*/
+						if(!empty($arRez)):?>
+							<div class="a-new">
+								<strong>Хотите опубликовать свое объявление?</strong>
+								<a class="btn green" href="">Подать объявление</a>
+							</div>
+					<?else:?>		
+						<div class="a-new">
+							<strong>
+							Хотите опубликовать свое объявление?
+							<a href="">Войдите</a>
+							или
+							<a href="">зарегистрируйтесь</a>
+							</strong>
+						</div>
+					<?endif?>	
 				</div>
 			</div>
 		</div>
